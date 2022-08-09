cd672bb22e066298558196337e48bdfa0610408b
Gabriel Torres <gbrtorress@gmail.com>
2022-08-09 09:11:17 -0300

    att

--- contador/js/script.js
+++ contador/js/script.js
@@ -4,12 +4,17 @@ function contar() {
     let passo = document.getElementById("txtp")
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = "Impossivel contar !!"
         window.alert("[ERROR] Faltam Dados !!")
     } else {
-        res.innerHTML = "Contando:"
+        res.innerHTML = "Contando: <br>"
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert("Passo inválido! Considerando PASSO 1")
+            p = 1
+        }
         if (i < f) {
             for(let c = i; c <= f; c += p) {
                 res.innerHTML += ` ${c} \u{1f449}`
